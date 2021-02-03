f9da7b536d8b8fdf284c0bf7123c2390e465c3c6
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-03 18:29:44 -0500

    add componentDidMount

--- src/components/newsSearch/NewsSearch.jsx
+++ src/components/newsSearch/NewsSearch.jsx
@@ -7,6 +7,13 @@ export default class NewsSearch extends Component {
       article: ''
     }
 
+    componentDidMount() {
+      // eslint-disable-next-line max-len
+      fetch(`https://newsapi.org/v2/everything?q=tesla&apiKey=${process.env.API_KEY}`)
+      .then(res => res.json())
+      .then(console.log)
+    }
+
     handleChange = ({ target }) => {
       this.setState({ article: target.value });
     }
