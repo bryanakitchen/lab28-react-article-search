4b31fa6a204a6ae832205c1fa2c3efc367896c72
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-03 19:13:50 -0500

    add html template for Article

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Article() {
-  return (
-    <div>
-            Hello from the Article component.
-    </div>
-  );
-}
+const Article = ({ title, author, description  }) => {
+  <>
+    <h2>{title}</h2>
+    <h3>{author}</h3>
+    <p>{description}</p>
+  </>
+};
+
+Article.prototypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+    
+export default Article;
--- src/components/articleList/ArticleList.jsx
+++ src/components/articleList/ArticleList.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const ArticleList = ({ articles }) => {
   const articleList = articles.map(article => (
     <li key={article.url}>
-      <Article articles={articles} />
+      <Article {...articles} />
     </li>
   ));
 
